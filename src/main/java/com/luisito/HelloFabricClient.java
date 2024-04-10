b9c4cf4ace03a3a196773ac2c10db39a2ceb82c6
Luis <luis.villela021@gmail.com>
2024-04-10 16:29:27 -0500

    aaaaa

---
+++ src/main/java/com/luisito/HelloFabricClient.java
@@ -0,0 +1,12 @@
+package com.luisito;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
