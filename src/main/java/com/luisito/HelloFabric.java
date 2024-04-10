a7ea736f07ee44657a9d20fafb3d1935ca3ae5ab
Luis <luis.villela021@gmail.com>
2024-04-10 16:08:27 -0500

    aaaaa

---
+++ src/main/java/com/luisito/HelloFabric.java
@@ -0,0 +1,18 @@
+package com.luisito;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloFabric implements ModInitializer {
+
+	public static final String MOD_ID = "hellofabric";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+
+}
